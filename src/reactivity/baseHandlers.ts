5868a5dea3970400e8754bc54e9d5a0fee7c8c23
秋簌 <1048097942@qq.com>
2023-02-02 13:02:29 +0800

    新增判断readonloy

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -2,7 +2,8 @@ import { track, trigger } from './effect'
 import { ReactiveFlags } from './reactive'
 export const createGetter = (isReadonly = false) => {
   return function get(target, key) {
-    if (key === ReactiveFlags.IS_REACTIVE) return true
+    if (key === ReactiveFlags.IS_REACTIVE) return !isReadonly
+    else if (key === ReactiveFlags.IS_READONLY) return isReadonly
     const res = Reflect.get(target, key)
     if (!isReadonly) track(target, key)
     return res
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,7 +1,8 @@
 import { mutableHandler, readonlyHandler } from './baseHandlers'
 
 export const enum ReactiveFlags {
-  IS_REACTIVE = '__v_isReactive'
+  IS_REACTIVE = '__v_isReactive',
+  IS_READONLY = '__v_isReadonly'
 }
 /**响应式对象实现 */
 export const createActiveObject = (raw, baseHandlers) => new Proxy(raw, baseHandlers)
@@ -9,3 +10,5 @@ export const reactive = raw => createActiveObject(raw, mutableHandler)
 export const readonly = raw => createActiveObject(raw, readonlyHandler)
 
 export const isReactive = raw => !!raw[ReactiveFlags.IS_REACTIVE]
+
+export const isReadonly = raw => !!raw[ReactiveFlags.IS_READONLY]
